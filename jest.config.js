d1647fb61a6490985edaf71003755a5039571782
Alexis Bollia <alexis@bollia.fr>
2022-08-25 18:05:10 +0200

    Add javascript template

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,19 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: 'ts-jest/presets/js-with-ts-esm',
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json"
+    }
+  },
+  moduleFileExtensions: [
+    "ts",
+    "js"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  testMatch: [
+    "**/test/**/*.test.(ts|js)"
+  ],
+  testEnvironment: "node"
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const helloWorld = () : string => {
+const helloWorld = () => {
     return "Hello World"
 }
 
